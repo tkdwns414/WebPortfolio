68eecbab509c6a2ec45b438c1987c09cbfbf2f82
tkdwns414 <sangjune2000@naver.com>
2022-02-18 17:01:35 +0900

    initial project upload

---
+++ WebPo/src/main/java/backend/WebPo/WebPoApplication.java
@@ -0,0 +1,13 @@
+package backend.WebPo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebPoApplication.class, args);
+	}
+
+}
---
+++ WebPo/src/test/java/backend/WebPo/WebPoApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.WebPo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
