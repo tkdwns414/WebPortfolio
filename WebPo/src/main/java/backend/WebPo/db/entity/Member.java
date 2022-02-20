db637159c7b028f7450c7309fbee19d7466b64d7
tkdwns414 <sangjune2000@naver.com>
2022-02-20 21:53:51 +0900

    Feat: update Member Entity & MemberRepository

--- WebPo/src/main/java/backend/WebPo/db/entity/Member.java
+++ WebPo/src/main/java/backend/WebPo/db/entity/Member.java
@@ -1,11 +1,16 @@
 package backend.WebPo.db.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@Getter
+@Setter
 public class Member {
 
     @Id @GeneratedValue
--- WebPo/src/main/java/backend/WebPo/db/repository/MemberRepository.java
+++ WebPo/src/main/java/backend/WebPo/db/repository/MemberRepository.java
@@ -4,4 +4,6 @@ import backend.WebPo.db.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    public Member findByLoginId(String loginId);
 }
