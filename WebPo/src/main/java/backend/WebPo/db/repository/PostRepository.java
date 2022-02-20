6ce9b242d07662f33143412dde3c6da6d72261cb
tkdwns414 <sangjune2000@naver.com>
2022-02-21 03:15:16 +0900

    Feat: update PostRepository

--- WebPo/src/main/java/backend/WebPo/db/repository/PostRepository.java
+++ WebPo/src/main/java/backend/WebPo/db/repository/PostRepository.java
@@ -1,7 +1,11 @@
 package backend.WebPo.db.repository;
 
+import backend.WebPo.db.entity.Member;
 import backend.WebPo.db.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+    public List<Post> findByMember(Member member);
 }
