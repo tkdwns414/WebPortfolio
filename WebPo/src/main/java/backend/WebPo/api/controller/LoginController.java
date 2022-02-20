77d42e5f424f26ae814d0de8bf57c590ee009060
tkdwns414 <sangjune2000@naver.com>
2022-02-20 21:51:44 +0900

    Feat: LoginController V1

--- WebPo/src/main/java/backend/WebPo/api/controller/LoginController.java
+++ WebPo/src/main/java/backend/WebPo/api/controller/LoginController.java
@@ -1,11 +1,52 @@
 package backend.WebPo.api.controller;
 
+import backend.WebPo.api.request.LoginRequest;
+import backend.WebPo.api.response.BaseResponse;
+import backend.WebPo.db.entity.Member;
+import backend.WebPo.db.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class LoginController {
+
+    private final MemberRepository memberRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody LoginRequest loginRequest, HttpServletRequest request){
+        String loginId = loginRequest.getLoginId();
+        String loginPw = loginRequest.getLoginPw();
+
+        Member member = memberRepository.findByLoginId(loginId);
+
+        if (member == null){
+            return ResponseEntity.status(404).body(BaseResponse.of(404,"Not Existing Member"));
+        }
+        if(passwordEncoder.matches(loginPw, member.getLoginPw())){
+            HttpSession session = request.getSession();
+            session.setAttribute("LOGIN_MEMBER", member);
+            return ResponseEntity.status(200).body(BaseResponse.of(200,"Login Success"));
+        }
+        return ResponseEntity.status(401).body(BaseResponse.of(401, "Invalid Password"));
+    }
+
+    @GetMapping("/logout")
+    ResponseEntity logout(HttpServletRequest request){
+        HttpSession session = request.getSession(false);
+        if(session != null){
+            session.invalidate();
+        }
+        return ResponseEntity.status(200).body(BaseResponse.of(200,"Logout Success"));
+    }
 }
